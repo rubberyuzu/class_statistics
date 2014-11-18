5e6153bdf0f49bf345b184e8658e34cd9e6e1ef3
rubberyuzu <0696338342@mail.ecc.u-tokyo.ac.jp>
2014-11-18 11:30:09 +0900

    changed

--- various_normal_distributions.r
+++ various_normal_distributions.r
@@ -1,18 +1,15 @@
 x <- seq(-3, 73.5, 0.01)
 
 x.ncurve1 <- dnorm(x, mean=20, sd=4)
-x.ncurve1 <- dnorm(x, mean=0, sd=1)
-x.ncurve1 <- dnorm(x, mean=40, sd=2)
-x.ncurve1 <- dnorm(x, mean=60, sd=4.5)
+x.ncurve2 <- dnorm(x, mean=0, sd=1)
+x.ncurve3 <- dnorm(x, mean=40, sd=2)
+x.ncurve4 <- dnorm(x, mean=60, sd=4.5)
 
-plot(x, x.ncurve1, xlab="x", ylab="x.norm", xlim=c(-3, 73.5),
-	ylim=c(0, 0.45), type="1", col="blue")
+plot(x, x.ncurve1, xlab="x", ylab="x.norm", axes=F, xlim=c(-3, 73.5),ylim=c(0, 0.45), type="1", col="blue")
 par(new=T)
-plot(x, x.ncurve2, xlab="", ylab="", axes=F, xlim=c(-3, 73.5),
-	ylim=c(0, 0.45), type="1", col="red")
+plot(x, x.ncurve2, xlab="", ylab="", axes=F, xlim=c(-3, 73.5),ylim=c(0, 0.45), type="1", col="red")
 par(new=T)
-plot(x, x.ncurve3, xlab="", ylab="", axes=F, xlim=c(-3, 73.5),
-	ylim=c(0, 0.45), type="1", col="green")
+plot(x, x.ncurve3, xlab="", ylab="", axes=F, xlim=c(-3, 73.5),ylim=c(0, 0.45), type="1", col="green")
 par(new=T)
-plot(x, x.ncurve4, xlab="", ylab="", axes=F, xlim=c(-3, 73.5),
-	ylim=c(0, 0.45), type="1", col="orange")
+plot(x, x.ncurve4, xlab="", ylab="", axes=F, xlim=c(-3, 73.5),ylim=c(0, 0.45), type="1", col="orange")
+	
